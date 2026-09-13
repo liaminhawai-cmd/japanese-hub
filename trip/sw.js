948f9b0439041d7c04da71e377a6aeae51f1bd4c
Andrew Cerini <08598176@kew.vic.edu.au>
2026-09-13 15:48:14 +1000

    Trip app: improve itinerary text readability (v29)

--- trip/sw.js
+++ trip/sw.js
@@ -16,7 +16,7 @@
    Bump CACHE when the app changes, or phones will keep serving the old copy.
    ========================================================================== */
 
-var CACHE = "japan-trip-v28";
+var CACHE = "japan-trip-v29";
 
 var SHELL = [
   "./",
